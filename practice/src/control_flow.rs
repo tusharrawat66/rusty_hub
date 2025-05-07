16214be32b51ba2d1b0775fc01efac1556b0d23d
tusharrawat66 <tusharrawat66@gmail.com>
2025-05-08 00:43:53 +0530

    exception handled in control file

--- practice/src/control_flow.rs
+++ practice/src/control_flow.rs
@@ -1,13 +1,25 @@
+use std::io;
+
+
 pub fn if_test(){
     let age_to_drive:u8 = 18u8;
     println!("Enter the person's age:");
 
     let my_input = &mut String::from("");
-    std::io::stdin().read_line(my_input).unwrap();
+    io::stdin().read_line( my_input).expect("Failed to read input");
+      
+            match my_input.trim().parse::<u8>(){
+                Ok(age) =>{
+                    if age >= age_to_drive{
+                        println!("Issuing driver's license");   
+                    }else{
+                        println!("Not eligible for a license")
+                }
+            } 
+            Err(_) => println!("Invalid input! Please enter a valid number (0-255).")
 
-    let age: u8 = my_input.replace("\n", "").parse::<u8>().unwrap();
-    if age >= age_to_drive{
-        println!("Issuing driver's license");   
+        }
     }
-    else{println!("They can Fuck-off")}
-}
+
+
+    
